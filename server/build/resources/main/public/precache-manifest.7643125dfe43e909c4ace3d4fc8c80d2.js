caa2cfc3ac134a7b57ef058636dcf185f5dca107
Chris Downer <62905338+cadowner90@users.noreply.github.com>
2020-04-20 15:37:03 -0600

    Add files via upload

---
+++ server/build/resources/main/public/precache-manifest.7643125dfe43e909c4ace3d4fc8c80d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "d01d978a16aaa609b11c",
+    "url": "/static/js/main.d01d978a.chunk.js"
+  },
+  {
+    "revision": "e655e75c4b1a3e601196",
+    "url": "/static/js/2.e655e75c.chunk.js"
+  },
+  {
+    "revision": "d01d978a16aaa609b11c",
+    "url": "/static/css/main.6edd71c2.chunk.css"
+  },
+  {
+    "revision": "207d159bad63646aabecc904bfeb6562",
+    "url": "/index.html"
+  }
+];
