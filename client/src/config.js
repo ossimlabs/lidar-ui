cd64561e15ce851f535a4996e811909f7aa988fd
melbsurfer <melbsurfer@gmail.com>
2020-11-30 13:51:51 -0500

    Modified context path

--- client/src/config.js
+++ client/src/config.js
@@ -1,10 +1,14 @@
 const prod = process.env.NODE_ENV === "production";
 console.log(`Loading ${process.env.NODE_ENV} config...`);
 
-export const CONTEXT_PATH = window.location.pathname
-  .split("/")
-  .slice(0, -2)
-  .join("/");
+export let CONTEXT_PATH = window.location.pathname;
+
+console.log(window.location.path);
+console.log(`Loading the following context: ${CONTEXT_PATH}`);
+
+if (!prod && CONTEXT_PATH === "/") {
+  CONTEXT_PATH = "";
+}
 
 export const SERVER_URL = prod
   ? CONTEXT_PATH
